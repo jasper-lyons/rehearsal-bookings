fa1fbf8a4f09fb43dcb617b016fa0f64d4e19787
Jasper Lyons <jasper.lyons@gmail.com>
2024-12-26 10:58:12 +0000

    Skeleton the tempalte rendering api

--- web/templates/main.go
+++ web/templates/main.go
@@ -1,6 +1,23 @@
 package templates
 
-import "embed"
+import (
+	"embed"
+	"net/http"
+	"log"
+	"html/template"
+)
 
 //go:embed *
 var TemplateFiles embed.FS
+
+func Render(rw http.ResponseWriter, templateString string, context any) error {
+	bytes, err := TemplateFiles.ReadFile(templateString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	t, err := template.New(templateString).Parse(string(bytes))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return t.Execute(rw, context)
+}
