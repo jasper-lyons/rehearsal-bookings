e0dc7dffd584dc54cc790d76077be34895c6877c
Jasper Lyons <jasper.lyons@gmail.com>
2024-12-26 10:12:06 +0000

    Basic template rendering

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,8 +3,16 @@ package main
 import (
 	"net/http"
 	"log"
-	"fmt"
 	"os"
+	// GO Embed doesn't support embedding files from outside the module boundary
+	// (in this case, anything outside of this directory) but we want to store
+	// templates and static files at the route directory so we need to treat them
+	// as their own go modules (you'll see the main.go files in the web/static and
+	// web/templates directories) so that we can import them here and access the 
+	// embedded static files!
+	// "rehearsal-bookings/web/static"
+	tmpl "rehearsal-bookings/web/templates"
+	"html/template"
 )
 
 func EnvOrDefault(key string, fallback string) string {
@@ -17,7 +25,15 @@ func EnvOrDefault(key string, fallback string) string {
 
 func main() {
 	http.HandleFunc("GET /", func (rw http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(rw, "Hello!")
+		bytes, err := tmpl.TemplateFiles.ReadFile("bookings.html.tmpl")
+		if err != nil {
+			log.Fatal(err)
+		}
+		t, err := template.New("bookings").Parse(string(bytes))
+		if err != nil {
+			log.Fatal(err)
+		}
+		t.Execute(rw, nil)
 	})
 
 	log.Fatal(http.ListenAndServe(EnvOrDefault("PORT", ":8080"), nil))
