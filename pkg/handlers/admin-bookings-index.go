9a22b8e681473a077a831a8f60e2460ec98af26b
Mila M Lyons <mila.thought@gmail.com>
2025-03-09 21:32:13 +0000

    Making an interactive admin panel showing all the information for booking

--- pkg/handlers/admin-bookings-index.go
+++ pkg/handlers/admin-bookings-index.go
@@ -3,19 +3,53 @@ package handlers
 import (
 	"net/http"
 	da "rehearsal-bookings/pkg/data_access"
+	"sort"
 )
 
 type AdminBookingsIndexView struct {
+	GroupedBookings []GroupedBookings
+}
+
+type GroupedBookings struct {
+	Date     string
 	Bookings []da.Booking
 }
 
 func AdminBookingsIndex(br *da.BookingsRepository[da.StorageDriver]) Handler {
-	return Handler(func (w http.ResponseWriter, r *http.Request) Handler {
+	return Handler(func(w http.ResponseWriter, r *http.Request) Handler {
 		bookings, err := br.All()
 		if err != nil {
 			return Error(err, 500)
 		}
 
-		return Template("admin-bookings-index.html.tmpl", AdminBookingsIndexView{Bookings: bookings})
+		sort.Slice(bookings, func(i, j int) bool {
+			return bookings[i].StartTime.Before(bookings[j].StartTime)
+		})
+
+		// Group bookings by date
+		groupedBookings := groupBookingsByDate(bookings)
+
+		return Template("admin-bookings-index.html.tmpl", AdminBookingsIndexView{GroupedBookings: groupedBookings})
 	})
+}
+
+// function to group bookings by date - called from AdminBookingsIndex
+func groupBookingsByDate(bookings []da.Booking) []GroupedBookings {
+	grouped := make(map[string][]da.Booking)
+	for _, booking := range bookings {
+		date := booking.StartTime.Format("2006-01-02")
+		grouped[date] = append(grouped[date], booking)
+	}
+
+	var groupedBookings []GroupedBookings
+	for date, bookings := range grouped {
+		groupedBookings = append(groupedBookings, GroupedBookings{Date: date, Bookings: bookings})
+	}
+
+	// Sort grouped bookings by date
+	sort.Slice(groupedBookings, func(i, j int) bool {
+		return groupedBookings[i].Date < groupedBookings[j].Date
+	})
+
+	return groupedBookings
 }
