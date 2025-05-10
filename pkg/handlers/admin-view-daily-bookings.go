34432ea0e46f2f9150a12796e266de079f666996
Jasper Lyons <jasper.lyons@gmail.com>
2025-05-10 15:13:19 +0100

    add ui for codes messages

--- pkg/handlers/admin-view-daily-bookings.go
+++ pkg/handlers/admin-view-daily-bookings.go
@@ -3,53 +3,105 @@ package handlers
 import (
 	"net/http"
 	da "rehearsal-bookings/pkg/data_access"
-	"sort"
+	"time"
+	"text/template"
+	"bytes"
 )
 
 type AdminBookingsDailyView struct {
-	GroupedBookings []GroupedBookings
+	Date     string
+	Bookings []AdminBooking
 }
 
-type GroupedBookings struct {
-	Date     string
-	Bookings []da.Booking
+type AdminBooking struct {
+	da.Booking
+	BookingCodesMessage string
+}
+
+func NewAdminBooking(booking da.Booking, codesMessage string) AdminBooking {
+	return AdminBooking {
+		Booking: booking,
+		BookingCodesMessage: codesMessage,
+	}
+}
+
+type CustomerBookingCodesSMSData struct {
+	Room string
+	StartTime string 
+	EndTime string 
+	FrontDoorCode string
+	RoomDoorCode string
+	Cymbals int64
 }
 
 func AdminViewDailyBookings(br *da.BookingsRepository[da.StorageDriver]) Handler {
 	return Handler(func(w http.ResponseWriter, r *http.Request) Handler {
-		bookings, err := br.Where("status IN ('paid', 'unpaid', 'hold', 'cancelled') ")
-		if err != nil {
-			return Error(err, 500)
+		dateParam := r.URL.Query().Get("date")
+		var selectedDate time.Time
+		if dateParam == "" {
+			// TODO: redirect to todays date with the date param default to today
+			selectedDate = time.Now()
+		} else {
+			var err error
+			selectedDate, err = time.Parse("2006-01-02", dateParam)
+			if err != nil {
+				return Error(err, http.StatusBadRequest)
+			}
 		}
 
-		sort.Slice(bookings, func(i, j int) bool {
-			return bookings[i].StartTime.Before(bookings[j].StartTime)
+		// Fetch all bookings for the selected date
+		startOfDay := selectedDate.Truncate(24 * time.Hour)
+		endOfDay := startOfDay.Add(24 * time.Hour)
+
+		bookings, err := br.Where("status IN ('paid', 'unpaid', 'hold') and start_time >= ? and end_time <= ?", startOfDay, endOfDay)
+		if err != nil {
+			http.Error(w, "Failed to fetch bookings", http.StatusInternalServerError)
+			return nil
+		}
+
+		adminBookings := make([]AdminBooking, len(bookings))
+		customerCodesMessageTemplate := `
+Hey, we are looking forward to seeing you at Bad Habit for your rehearsal today!
+
+Here are the details and information about your booking:
+• Booking time: {{ .StartTime }}-{{ .EndTime }} 
+• Location: {{ .Room }}
+• Front door access code: {{ .FrontDoorCode }}#
+• Room door access code: {{ .RoomDoorCode }}# (Please note that the keypad to use for accessing rooms is the one found on the wall rather than on the door)
+{{ if eq .Cymbals 1 }}
+
+You asked for Cymbals so they'll be left in the room :)
+{{ end }}
+Any questions or concerns, please get in touch!
+		`
+		for i, booking := range bookings {
+			messageTmpl, err := template.New("codes-sms").Parse(customerCodesMessageTemplate)
+			if err != nil {
+				return Error(err, http.StatusInternalServerError)
+			}
+
+			customerCodesMessageData := CustomerBookingCodesSMSData {
+				Room: booking.RoomName,
+				StartTime: booking.StartTime.Format("15:04"),
+				EndTime: booking.EndTime.Format("15:04"),
+				FrontDoorCode: "",
+				RoomDoorCode: "",
+				Cymbals: booking.Cymbals,
+			}
+
+			var messageContent bytes.Buffer
+			if err := messageTmpl.Execute(&messageContent, customerCodesMessageData); err != nil {
+				return Error(err, http.StatusInternalServerError)
+			}
+			adminBookings[i] = AdminBooking {
+				Booking: booking,
+				BookingCodesMessage: messageContent.String(),
+			}
+		}
+
+		return Template("admin-view-daily-bookings.html.tmpl", AdminBookingsDailyView {
+			Date: selectedDate.Format("2006-01-02"),
+			Bookings: adminBookings,
 		})
-
-		// Group bookings by date
-		groupedBookings := groupBookingsByDate(bookings)
-
-		return Template("admin-view-daily-bookings.html.tmpl", AdminBookingsDailyView{GroupedBookings: groupedBookings})
 	})
-}
-
-// function to group bookings by date - called from AdminBookingsIndex
-func groupBookingsByDate(bookings []da.Booking) []GroupedBookings {
-	grouped := make(map[string][]da.Booking)
-	for _, booking := range bookings {
-		date := booking.StartTime.Format("2006-01-02")
-		grouped[date] = append(grouped[date], booking)
-	}
-
-	var groupedBookings []GroupedBookings
-	for date, bookings := range grouped {
-		groupedBookings = append(groupedBookings, GroupedBookings{Date: date, Bookings: bookings})
-	}
-
-	// Sort grouped bookings by date
-	sort.Slice(groupedBookings, func(i, j int) bool {
-		return groupedBookings[i].Date < groupedBookings[j].Date
-	})
-
-	return groupedBookings
 }
